078dfb5e3276e45288ae6e7920f798c75945f7e3
Code Sandbox <hello@codesandbox.io>
2022-09-17 17:17:01 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e74a97b920170878bc0a10912b646f25.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c673da724ba5b3524d93145c05ac843",
+    "url": "https://bis29abena.github.io/csb-95yg1g/index.html"
+  },
+  {
+    "revision": "6a019708122ef2195904",
+    "url": "https://bis29abena.github.io/csb-95yg1g/static/js/2.fd03c0c9.chunk.js"
+  },
+  {
+    "revision": "da3b165a277b075024c9",
+    "url": "https://bis29abena.github.io/csb-95yg1g/static/js/main.e88e1341.chunk.js"
+  },
+  {
+    "revision": "a9fcde0712c6ee7ee1f2",
+    "url": "https://bis29abena.github.io/csb-95yg1g/static/js/runtime-main.1d7c8411.js"
+  }
+]);
